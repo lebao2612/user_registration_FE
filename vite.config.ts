ff823841161484ecdc3a2acc637f492967efe9ef
lebao2612 <btaldforever@gmail.com>
2025-10-28 11:38:48 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // Dành cho khi bạn chạy `npm run dev`
+    // không ảnh hưởng đến build production, nhưng vẫn nên có
+    open: true,
+  },
 })
